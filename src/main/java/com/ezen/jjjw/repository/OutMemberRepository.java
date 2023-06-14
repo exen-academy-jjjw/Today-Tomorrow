4a5ead17fcac9fbd794d29f3cc701b7ad9d98e90
user <yjl3762@gmail.com>
2023-06-14 11:15:19 +0900

    회원탈퇴 기능 추가

---
+++ src/main/java/com/ezen/jjjw/repository/OutMemberRepository.java
@@ -0,0 +1,7 @@
+package com.ezen.jjjw.repository;
+
+import com.ezen.jjjw.domain.entity.OutMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OutMemberRepository extends JpaRepository<OutMember, Long> {
+}
