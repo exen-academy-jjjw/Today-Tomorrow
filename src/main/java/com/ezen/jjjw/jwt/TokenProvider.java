4796086f265a8e016a9e22a5dc1192b37f996df4
jia-son <116625758+jia-son@users.noreply.github.com>
2023-06-20 11:59:21 +0900

    띄어쓰기 수정

--- src/main/java/com/ezen/jjjw/jwt/TokenProvider.java
+++ src/main/java/com/ezen/jjjw/jwt/TokenProvider.java
@@ -121,13 +121,13 @@ public class TokenProvider {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
             return true;
         } catch (SecurityException | MalformedJwtException e) {
-            log.info("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+            log.info("Invalid JWT signature, 유효하지 않는 JWT 서명입니다.");
         } catch (ExpiredJwtException e) {
             log.info("Expired JWT token, 만료된 JWT token 입니다.");
         } catch (UnsupportedJwtException e) {
-            log.info("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+            log.info("Unsupported JWT token, 지원되지 않는 JWT 토큰입니다.");
         } catch (IllegalArgumentException e) {
-            log.info("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+            log.info("JWT claims is empty, 잘못된 JWT 토큰입니다.");
         }
         return false;
     }
